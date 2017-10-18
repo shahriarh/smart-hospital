b6653add44429076f603981abe4fbf6559e5e79f
Shahriar Hossain <mossaddak31@gmail.com>
2017-10-18 20:48:48 +0600

    Yii Basic Framework as Skeleton
    FIrst Commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=smart-hospital',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
