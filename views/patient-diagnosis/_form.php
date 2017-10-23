bdd9daaf7ccf7db4b95e21522c3267fe45f3e2f6
Shahriar Hossain <mossaddak31@gmail.com>
2017-10-23 12:44:42 +0600

    aaaaa

--- views/patient-diagnosis/_form.php
+++ views/patient-diagnosis/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Diagnosis;
+use app\models\Prescription;
+use app\models\Patient;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\PatientDiagnosis */
@@ -13,12 +16,23 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'diagnosis_id')->textInput() ?>
-
-    <?= $form->field($model, 'prescription_id')->textInput() ?>
+	<?php echo $form->field($model, "diagnosis_id")->dropdownList(
+		Diagnosis::find()->select(['name', 'id'])->asArray()->indexBy('id')->column(),
+		['prompt'=>'Select Diagnosis']
+		); ?>
+	<?= $form->field($model, 'prescription_id')->textInput() ?>
+	<?php echo $form->field($model, "prescription_id")->dropdownList(
+		Prescription::find()->select(['name', 'id'])->asArray()->indexBy('id')->column(),
+		['prompt'=>'Select Prescription']
+		); ?>
 
     <?= $form->field($model, 'created_at')->textInput() ?>
 
     <?= $form->field($model, 'patient_id')->textInput() ?>
+	<?php echo $form->field($model, "patient_id")->dropdownList(
+		Patient::find()->select(['name', 'id'])->asArray()->indexBy('id')->column(),
+		['prompt'=>'Select Patient']
+		); ?>
 
     <?= $form->field($model, 'status')->dropDownList([ 'diag_pending' => 'Diag pending', 'diag_complete' => 'Diag complete', 'rep_pending' => 'Rep pending', 'rep_published' => 'Rep published', ], ['prompt' => '']) ?>
 
